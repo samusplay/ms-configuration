c4787cf112f872d6ebe60e1437d516b9ef967aac
Xertz <stiven131bee@gmail.com>
2026-04-16 21:38:11 -0500

    Se agrego healt check

--- app/routers/api.py
+++ app/routers/api.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 
 #enrutador
-api_router=APIRouter()
+api_router=APIRouter(prefix="/api/v1/configuration")
+
+@api_router.get("/health", tags=["Sistema"])
+def health():
+    return {"status": "ok", "service": "ms-CONFIGURATION"}
 
 #Registrar Rutas
